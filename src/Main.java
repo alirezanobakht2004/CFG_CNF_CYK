78b8cec741b75727ef82ebd1ebf11709abdc2603
alirezanobakht2004 <alireza.nobakht2004@gmail.com>
2024-01-30 15:37:40 +0330

    Third

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello d!");
+        HashMap<String, ArrayList<ArrayList<String>>> grammar = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String line;
+
+    }
+
+    private static Map.Entry<String, ArrayList<String>> parseProduction(String line) {
+        String[] parts = line.split("->");
+        if (parts.length == 2) {
+            String key = parts[0].trim();
+            ArrayList<String> value = new ArrayList<>(Arrays.asList(parts[1].trim().split(",")));
+            return new AbstractMap.SimpleEntry<>(key, value);
+        }
+        return null;
     }
 }
